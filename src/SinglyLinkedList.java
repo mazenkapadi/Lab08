b9077df40d0f83333394a5caa24855000c4cd1c6
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-04-03 11:19:35 -0400

    Initial commit

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,80 @@
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    private Node head;
+    private Node tail;
+
+    public void LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(Node newNode) {
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void prepend(Node newNode) {
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void insertAfter(Node currentNode, Node newNode) {
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else if (currentNode == tail) {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        else {
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void removeAfter(Node currentNode) {
+        if (currentNode == null && head != null) {
+            // Special case: remove head
+            Node succeedingNode = head.next;
+            head = succeedingNode;
+            if (succeedingNode == null) {
+                // Last item was removed
+                tail = null;
+            }
+        }
+        else if (currentNode.next != null) {
+            Node succeedingNode = currentNode.next.next;
+            currentNode.next = succeedingNode;
+            if (succeedingNode == null) {
+                // Remove tail
+                tail = currentNode;
+            }
+        }
+    }
+}
---
+++ src/primeNumbers.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class primeNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to look up until: ");
+        int n = sc.nextInt();
+        SinglyLinkedList primes = new SinglyLinkedList();
+        SinglyLinkedList primesWithThree = new SinglyLinkedList();
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                primes.append(new Node(i));
+                if (containsDigitThree(i)) {
+                    primesWithThree.append(new Node(i));
+                }
+            }
+        }
+
+        int sum = 0;
+        Node current = primesWithThree.getHead();
+        while (current != null) {
+            sum += current.data;
+            current = current.next;
+        }
+        System.out.println("Sum of primes with digit 3: " + sum);
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n)/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean containsDigitThree(int n) {
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit == 3) {
+                return true;
+            }
+            n /= 10;
+        }
+        return false;
+    }
+}
